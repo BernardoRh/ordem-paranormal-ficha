38a7f76a2ac9bc6b682063e99eb40d04b5c6b92f
Bernardo R. Horbach <bernardo.horbach@gmail.com>
2023-02-16 10:14:17 -0300

    Changing the visibility of las rolls so it won't apears on the screan all the time, just when hovered

--- src/components/LastRolls/index.tsx
+++ src/components/LastRolls/index.tsx
@@ -1,10 +1,30 @@
+import { CaretRight } from 'phosphor-react'
+import { useState } from 'react'
 import styles from './lastRolls.module.css'
 
 
 
 export function LastsRolls() {
+
+    const [showHistory, setShowHistory] = useState(false);
+
+    function handleChangeShowHistory(show: boolean) {
+        setShowHistory(show)
+    }
+
+    console.log(showHistory)
+
     return(
-        <div className={styles.historyRollDiceContainer}>
+        <div
+            onMouseOver={() => {
+                handleChangeShowHistory(true)
+            }}
+            onMouseOut={() => {
+                handleChangeShowHistory(false)
+            }}
+            className={styles.historyRollDiceContainer}
+            style={ showHistory == true ? {marginLeft: 0} : {marginLeft: "-8.5rem"}}
+        >
             <div className={styles.historyRollDiceContainerContent}>
                 <div className={styles.resultLastRoll}></div>
                 <div className={styles.resultLastRoll}></div>
@@ -13,7 +33,17 @@ export function LastsRolls() {
                 <div className={styles.resultLastRoll}></div>
                 <div className={styles.resultLastRoll}></div>
                 <div className={styles.resultLastRoll}></div>
-                <div className={styles.resultLastRoll}></div>
+            </div>
+            <div
+                className={styles.arrowToHover}
+                onMouseOver={() => {
+                    handleChangeShowHistory(true)
+                }}
+                onMouseOut={() => {
+                    handleChangeShowHistory(false)
+                }}
+            >
+                <CaretRight size={32} weight="fill" />
             </div>
         </div>
     )
